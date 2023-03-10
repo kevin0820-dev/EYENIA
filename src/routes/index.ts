d3efec46439fcf1a66a3631bb8cca66b1ea9355c
Abdul Manan <39721062+codebymanan@users.noreply.github.com>
2023-03-10 23:22:21 +0500

    deleting old code

--- src/routes/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as PrivatePages } from './private';
-export { default as PublicPages } from './public';
--- src/routes/private.ts
+++
@@ -1,7 +0,0 @@
-import UserProfile from "../pages/UserProfile";
-
-const routes = [
-  { path: "/user-profile", component: UserProfile, exact: true },
-];
-
-export default routes;
